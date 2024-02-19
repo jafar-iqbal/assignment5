efcfb93b661bc5db8c8a4ca08e300f3b7ab5889b
Jafar Iqbal <Iqbal.jafer@ARTIC2.LOCAL>
2024-02-19 16:44:51 +0300

    next commit

--- script/script.js
+++ script/script.js
@@ -1,8 +1,29 @@
+// popup notification part
 let popup = document.getElementById("popup");
 
 function openPopup() {
-    popup.classList.add("open-popup");
+  popup.classList.add("open-popup");
 }
 function closePopup() {
-    popup.classList.remove("open-popup");
+  popup.classList.remove("open-popup");
+}
+// -------------------------------------
+let totalPrice = 0;
+const busSeat = document.querySelectorAll(".seat");
+
+for (let i = 0; i < busSeat.length; i++) {
+  const seat = busSeat[i];
+  seat.addEventListener("click", function () {
+    const title = seat.querySelector("h3").innerText;
+
+    const price = parseFloat(seat.querySelector("span").innerText);
+    const priceInject = document.getElementById("priceInject");
+    const p = document.createElement("p");
+    p.innerText = title + " " + "Economoy" + " " + price;
+      priceInject.appendChild(p);
+
+      // calculate price
+      totalPrice += price;
+      document.getElementById("totalPrice").innerText = totalPrice;
+  });
 }
